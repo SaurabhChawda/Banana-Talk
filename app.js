0cc437edd7f10bc9e29d4c35e93acc74d9ef9fc3
Saurabh chawda <saurabhchawda2000@gmail.com>
2021-09-21 11:11:13 +0530

    Update Comment

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate"); 
+var txtInput = document.querySelector("#txt-input"); //take input from input box 
+var outputDiv = document.querySelector("#output");   //Give input from input box
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"  //Server link 
 
+//GetTranslation call this for text with server link(4)
 function getTranslationURL(text){
     return serverURL +"?" +"text=" +text
 }
@@ -13,17 +14,17 @@ function errorHandler(error){
     alert("Something wrong with Server! Try Again")
 }
 
-function clickHandler(){
+function clickHandler(){      //2
     console.log("Button Clicked");
     var inputText =txtInput.value;
 
 
-fetch(getTranslationURL(inputText))
-     .then(response => response.json())
+fetch(getTranslationURL(inputText)) //call GetTranslation funtion (3)
+     .then(response => response.json())  //Get Response back from GetTranslation function and convert text into new language (5)
      .then(json =>{
          var translatedText = json.contents.translated;
          outputDiv.innerText = translatedText;
      })
      .catch(errorHandler)
     }
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler) //1
